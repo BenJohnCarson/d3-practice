fc510154f0f4e931b516606c07321a4aa5dc08c6
BenJohnCarson <benjohncarson@gmail.com>
2017-07-27 09:58:03 +0100

    finished experimenting with donuts

--- src/donutChart.js
+++ src/donutChart.js
@@ -6,9 +6,10 @@ function donutChart() {
         variable, // value in data that will dictate proportions on chart
         category, // compare data by
         padAngle, // effectively dictates the gap between slices
-        floatFormat = d3.format('.4r'),
-        cornerRadius, // sets how rounded the corners are on each slice
-        percentFormat = d3.format(',.2%');
+        labelRadius = 175,
+        // floatFormat = d3.format('.4r'),
+        cornerRadius; // sets how rounded the corners are on each slice
+        // percentFormat = d3.format(',.2%');
 
     function chart(selection){
         selection.each(function(data) {
@@ -21,7 +22,7 @@ function donutChart() {
             // creates a new pie generator
             var pie = d3.pie()
                 .value(function(d) { 
-                    return floatFormat(d[variable]); })
+                    return d[variable]; })
                 .sort(null);
 
             // contructs and arc generator. This will be used for the donut. The difference between outer and inner
@@ -36,6 +37,8 @@ function donutChart() {
             var outerArc = d3.arc()
                 .outerRadius(radius * 0.9)
                 .innerRadius(radius * 0.9);
+                // .outerRadius(radius)
+                // .innerRadius(radius);
             // ===========================================================================================
 
             // ===========================================================================================
@@ -64,7 +67,6 @@ function donutChart() {
                 // Animation for drawing each section
                 .transition().delay(function(d, i) { return i * 100;}).duration(1000)
                 .attrTween('d', function(d) {
-                    // console.log(d);
                     var i = d3.interpolate(d.startAngle, d.endAngle);
                     return function(t) {
                         d.endAngle = i(t);
@@ -75,25 +77,27 @@ function donutChart() {
             // ===========================================================================================
 
             // ===========================================================================================
+            
+            //------------- Original Label Functions
             // add text labels
+            console.log(svg.select('.labelName').selectAll('text'));
             var label = svg.select('.labelName').selectAll('text')
                 .data(pie)
               .enter().append('text')
                 .attr('dy', '.35em')
                 // For animation, sets initial opacity
-                .attr("fill-opacity", 0)
                 .html(function(d) {
                     // add "key: value" for given category. Number inside tspan is bolded in stylesheet.
-                    return d.data[category] + ': <tspan>' + percentFormat(d.data[variable]) + '</tspan>';
+                    return d.data[category] + ': <tspan>' + (d.data[variable]) + '</tspan>';
                 })
                 .attr('transform', function(d) {
 
                     // effectively computes the centre of the slice.
                     // see https://github.com/d3/d3-shape/blob/master/README.md#arc_centroid
-                    console.log(d);
                     var pos = outerArc.centroid(d);
 
                     // changes the point to be on left or right depending on where label is.
+
                     pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
                     return 'translate(' + pos + ')';
                 })
@@ -101,7 +105,8 @@ function donutChart() {
                     // if slice centre is on the left, anchor text to start, otherwise anchor to end
                     return (midAngle(d)) < Math.PI ? 'start' : 'end';
                 })
-                // Animates opacity of lines
+                // Animates opacity of labels
+                .attr("fill-opacity", 0)
               .transition().delay(function(d, i) { return i * 150;})
               .attr("fill-opacity", 1);
             // ===========================================================================================
@@ -115,16 +120,94 @@ function donutChart() {
               // For animation, sets initial opacity
               .style("opacity", 0) 
                 .attr('points', function(d) {
-
                     // see label transform function for explanations of these three lines.
                     var pos = outerArc.centroid(d);
                     pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
+                    // return [arc.centroid(d), outerArc.centroid(d), pos]
                     return [arc.centroid(d), outerArc.centroid(d), pos]
                 })
                 // Animates opacity of lines
               .transition().delay(function(d, i) { return i * 150;})
-              .style("opacity", 0.5)
+              .style("opacity", 0.5);
+
+            // arrangeLabels(svg.select('.labelName'), 'text')
             // ===========================================================================================
+            //-------- Alternate label functions
+            
+            /*
+            var enteringLabels = svg.select('.labelName').selectAll(".label").data(pie).enter();
+            var labelGroups = enteringLabels.append("g").attr("class", "label");
+            labelGroups.append("circle").attr({
+                x: 0,
+                y: 0,
+                r: 2,
+                fill: "#000",
+                transform: function (d, i) {
+                    centroid = arc.centroid(d);
+                    return "translate(" + arc.centroid(d) + ")";
+                },
+                    'class': "label-circle"
+            });
+
+            
+
+            var textLines = labelGroups.append("line").attr({
+                x1: function (d, i) {
+                    console.log("sefsefu");
+                    return arc.centroid(d)[0];
+                },
+                y1: function (d, i) {
+                    return arc.centroid(d)[1];
+                },
+                x2: function (d, i) {
+                    centroid = arc.centroid(d);
+                    midAngle = Math.atan2(centroid[1], centroid[0]);
+                    x = Math.cos(midAngle) * labelRadius;
+                    return x;
+                },
+                y2: function (d, i) {
+                    centroid = arc.centroid(d);
+                    midAngle = Math.atan2(centroid[1], centroid[0]);
+                    y = Math.sin(midAngle) * labelRadius;
+                    return y;
+                },
+                    'class': "polyline"
+            });
+            var textLabels = labelGroups.append("text").attr({
+                x: function (d, i) {
+                    console.log("d");
+                    var centroid = arc.centroid(d);
+                    var midAngle = Math.atan2(centroid[1], centroid[0]);
+                    var x = Math.cos(midAngle) * labelRadius;
+                    var sign = (x > 0) ? 1 : -1
+                    var labelX = x + (5 * sign)
+                    return labelX;
+                },
+                y: function (d, i) {
+                    var centroid = arc.centroid(d);
+                    var midAngle = Math.atan2(centroid[1], centroid[0]);1
+                    var y = Math.sin(midAngle) * labelRadius;
+                    console.log(y);
+                    return y;
+                },
+                    'text-anchor': function (d, i) {
+                    var centroid = arc.centroid(d);
+                    var midAngle = Math.atan2(centroid[1], centroid[0]);
+                    var x = Math.cos(midAngle) * labelRadius;
+                    return (x > 0) ? "start" : "end";
+                },
+                    'class': function(d){return'labelName'},
+                    'text': function (d) {
+                return d.data[category]
+            }
+            });
+            //     .html(function (d) {
+            //     return d.data[category]
+            // });
+
+                console.log(textLines);
+                console.log(textLabels);
+                */
 
             // ===========================================================================================
             // add tooltip to mouse events on slices and labels
@@ -174,7 +257,7 @@ function donutChart() {
                 for (var key in data.data) {
 
                     // if value is a number, format it as a percentage
-                    var value = (!isNaN(parseFloat(data.data[key]))) ? percentFormat(data.data[key]) : data.data[key];
+                    var value = data.data[key];
 
                     // leave off 'dy' attr for first tspan so the 'dy' attr on text element works. The 'dy' attr on
                     // tspan effectively imitates a line break.
@@ -185,10 +268,48 @@ function donutChart() {
 
                 return tip;
             }
-            // ===========================================================================================
+            // ==========================================================================================
+
+            function arrangeLabels(selection, label_class) {
+                console.log(selection.selectAll(label_class));
+                var move = 1;
+                while (move > 0) {
+                    move = 0;
+                    selection.selectAll(label_class)
+                        .each(function() {
+                            var that = this;
+                            var a = this.getBoundingClientRect();
+                            console.log(a);
+                            selection.selectAll(label_class)
+                                .each(function() {
+                                    if (this != that) {
+                                        var b = this.getBoundingClientRect();
+                                        if ((Math.abs(a.left - b.left) * 2 < (a.width + b.width)) && (Math.abs(a.top - b.top) * 2 < (a.height + b.height))) {
+                                            var dx = (Math.max(0, a.right - b.left) + Math.min(0, a.left - b.right)) * 0.01;
+                                            var dy = (Math.max(0, a.bottom - b.top) + Math.min(0, a.top - b.bottom)) * 0.02;
+                                            var tt = getTransformation(d3.select(this)
+                                                .attr("transform"));
+                                            var to = getTransformation(d3.select(that)
+                                                .attr("transform"));
+                                            move += Math.abs(dx) + Math.abs(dy);
+
+                                            to.translate = [to.translateX + dx, to.translateY + dy];
+                                            tt.translate = [tt.translateX - dx, tt.translateY - dy];
+                                            d3.select(this)
+                                                .attr("transform", "translate(" + tt.translate + ")");
+                                            d3.select(that)
+                                                .attr("transform", "translate(" + to.translate + ")");
+                                            a = this.getBoundingClientRect();
+                                        }
+                                    }
+                                });
+                        });
+                }
+}
 
         });
     }
+
 
     // getter and setter functions. See Mike Bostocks post "Towards Reusable Charts" for a tutorial on how this works.
     chart.width = function(value) {
